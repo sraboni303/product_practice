a93ec1257386d3e86c3d05363af6ae35141d4445
sraboni303 <sraboni391@gmail.com>
2021-04-20 13:44:08 +0600

    view product create page

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card p-5">
+                This is product create page
+            </div>
+
+            <div class="my-3">
+                <a href="#" class="btn btn-secondary">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
