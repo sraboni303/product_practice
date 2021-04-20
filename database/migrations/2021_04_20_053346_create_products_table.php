a89f9e5ee42d61f667ca84fd4eb7195163e2af71
sraboni303 <sraboni391@gmail.com>
2021-04-20 13:00:13 +0600

    migration of products

--- database/migrations/2021_04_20_053346_create_products_table.php
+++ database/migrations/2021_04_20_053346_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('subcategory_id');
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
